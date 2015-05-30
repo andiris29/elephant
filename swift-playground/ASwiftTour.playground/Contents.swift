b9452d89e45fcde7e799f6e6f520956124542923
andiris29 <andiris29@gmail.com>
2015-05-30 15:33:36 +0800

    try research kit

---
+++ swift-playground/ASwiftTour.playground/Contents.swift
@@ -0,0 +1,9 @@
+println("Hello world!")
+
+let s = "s";
+let i = 1;
+let comb1 = String(i) + s;
+let comb2 = i.description + s;
+
+let substitute1 = "a\(comb1)b";
+let substitute2 = "a" + \(comb2) + "b";
---
+++ swift-researchkit/try-researchkit/try-researchkit/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  try-researchkit
+//
+//  Created by Wang, Andrea on 5/28/15.
+//  Copyright (c) 2015 Wang, Andrea. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let myStep = ORKInstructionStep(identifier: "intro")
+        myStep.title = "Welcome to ResearchKit"
+        
+        let task = ORKOrderedTask(identifier: "task", steps: [myStep])
+        
+        let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: ORKTaskViewControllerDelegate {
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        let result = taskViewController.result;
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
